846858aa69208156f3b87131dd95a236225edd28
Marco Kellershoff <marco@mistweaverco.com>
2026-01-01 06:06:34 +0100

    fix(notify): enums + annotations

--- lua/bub/utils/notification/notify.lua
+++ lua/bub/utils/notification/notify.lua
@@ -1,29 +1,25 @@
 local config = require("bub.config")
+local enums = require("bub.enums")
 local is_not_empty = require("bub.utils.is_not.empty")
 local str_concat = require("bub.utils.str.concat")
 local stringify = require("bub.utils.convert.stringify")
-local types = require("bub.types")
 
 ---Generic notification function
 ---@module 'bub.utils.notify'
 ---@param message bub.Notification.Notify.Message Message of the notification
 ---@param level bub.Humans.LogLevel The level of the notification
----@params opts bub.Notification.Notify.Options|nil Options for the notification
+---@param opts bub.Notification.Notify.Options|nil Options for the notification
 return function(message, level, opts)
   opts = opts or {}
   local conf = config.get()
-  if conf.notify.provider == types.ConfigNotificationNotifyProvider.vim_notify then
-    vim.notify(stringify(message), types.Humans.LogLevel[level] or types.Humans.LogLevel.info, opts)
+  if conf.notify.provider == enums.Notification.Notify.Provider.vim_notify then
+    vim.notify(stringify(message), enums.Humans.LogLevel[level] or enums.Humans.LogLevel.info, opts)
     return
-  elseif conf.notify.provider == types.ConfigNotificationNotifyProvider.print then
+  elseif conf.notify.provider == enums.Notification.Notify.Provider.print then
     print(str_concat(level, stringify(message), is_not_empty(opts) and stringify(opts) or nil))
-  elseif type(conf.notify.provider) == types.ConfigNotificationNotifyProvider.custom then
+  elseif type(conf.notify.provider) == enums.Notification.Notify.Provider.custom then
     if conf.notify.custom_provider and type(conf.notify.custom_provider.notify) == "function" then
-      conf.notify.custom_provider.notify(
-        message,
-        types.HumanReadableLogLevel[level] or types.Humans.LogLevel.info,
-        opts
-      )
+      conf.notify.custom_provider.notify(message, enums.Humans.LogLevel[level] or enums.Humans.LogLevel.info, opts)
     else
       error("Custom notify provider is not properly configured.")
     end
